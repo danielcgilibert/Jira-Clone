378a1df3e6f3233e798391f12bb69e2c4bd8d9de
DanielCG <danielcgilibert@gmail.com>
2022-10-23 21:34:40 +0200

    :sparkles: Feat: post entries

--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { db } from '../../../database'
 import { Entry, IEntry } from '../../../models'
 
-type Data = { message: string } | IEntry[]
+type Data = { message: string } | IEntry[] | IEntry
 
 export default function handler(
   req: NextApiRequest,
@@ -11,7 +11,8 @@ export default function handler(
   switch (req.method) {
     case 'GET':
       return getEntries(res)
-
+    case 'POST':
+      return postEntry(req, res)
     default:
       return res.status(400).json({ message: 'Endpoint no existe' })
   }
@@ -23,4 +24,25 @@ const getEntries = async (res: NextApiResponse<Data>) => {
   await db.disconnect()
 
   res.status(200).json(entries)
+}
+
+const postEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { description = '' } = req.body
+  const newEntry = new Entry({
+    description,
+    createdAt: Date.now(),
+  })
+
+  try {
+    await db.connect()
+    await newEntry.save()
+    await db.disconnect()
+    return res.status(201).json(newEntry)
+  } catch (error) {
+    await db.disconnect()
+    console.error(error)
+    return res
+      .status(500)
+      .json({ message: 'Algo salio mal, mirar logs servidor' })
+  }
 }
