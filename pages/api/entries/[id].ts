2c338f1e0f3796d2bcf7b366d733349bdc7a6a34
DanielCG <danielcgilibert@gmail.com>
2022-10-24 13:31:35 +0200

    :sparkles: Feat: add getEntry endpoint

--- pages/api/entries/[id].ts
+++ pages/api/entries/[id].ts
@@ -17,6 +17,8 @@ export default function handler(
   switch (req.method) {
     case 'PUT':
       return updateEntry(req, res)
+    case 'GET':
+      return getEntry(req, res)
 
     default:
       return res.status(400).json({ message: 'El id no es válido' })
@@ -45,6 +47,23 @@ const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     )
     await db.disconnect()
     res.status(200).json(updateEntry!)
+  } catch (error: any) {
+    await db.disconnect()
+    res.status(400).json({ message: error.errors.status.message })
+  }
+}
+
+const getEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  try {
+    await db.connect()
+    const { id } = req.query
+    const entry = await Entry.findById(id)
+
+    if (!entry)
+      return res.status(400).json({ message: 'No hay entrada con ese ID' })
+
+    res.status(200).json(entry)
+    await db.disconnect()
   } catch (error: any) {
     await db.disconnect()
     res.status(400).json({ message: error.errors.status.message })
