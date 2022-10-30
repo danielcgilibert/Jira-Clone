89ea13cc5509344ce6ead43117699826aec182a7
DanielCG <danielcgilibert@gmail.com>
2022-10-30 20:15:14 +0100

    :recycle: Refactor: fix error text

--- database/db.ts
+++ database/db.ts
@@ -7,20 +7,20 @@
 
 import mongoose from 'mongoose'
 
-const mongooConnection = {
+const mongoConnection = {
   isConnected: 0,
 }
 
 export const connect = async () => {
-  if (mongooConnection.isConnected) {
+  if (mongoConnection.isConnected) {
     console.log('Connected')
     return
   }
 
   if (mongoose.connections.length > 0) {
-    mongooConnection.isConnected = mongoose.connections[0].readyState
+    mongoConnection.isConnected = mongoose.connections[0].readyState
 
-    if (mongooConnection.isConnected === 1) {
+    if (mongoConnection.isConnected === 1) {
       console.log('Usando conexión anterior')
     }
 
@@ -28,13 +28,13 @@ export const connect = async () => {
   }
 
   await mongoose.connect(process.env.MONGO_URL || '')
-  mongooConnection.isConnected = 1
+  mongoConnection.isConnected = 1
   console.log('Conectado a MongoDB', process.env.MONGO_URL)
 }
 
 export const disconnect = async () => {
   if (process.env.NODE_ENV === 'development') return
-  if (mongooConnection.isConnected === 0) return
+  if (mongoConnection.isConnected === 0) return
   await mongoose.disconnect()
   console.log('Desconectado de MongoDB')
 }
